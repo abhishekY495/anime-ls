3b8fa7966cdbdecd4892f573533699d226d6a68f
Abhishek <abhishekY495.sec@gmail.com>
2024-04-27 16:02:43 +0530

    Homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,125 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+import { UserDataContext } from "../contexts/UserDataContext";
+import animeCharacters from "../assets/anime-characters.jpg";
+import "./HomePage.css";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  const {
+    state: { userData },
+  } = useContext(UserDataContext);
+
+  const privateBtnHandler = () => localStorage.setItem("activeList", "private");
+  const publicBtnHandler = () => localStorage.setItem("activeList", "public");
+
+  return (
+    <Container>
+      <Row>
+        <Col
+          md={1}
+          className="m-auto mt-3 d-flex flex-column gap-3"
+          style={{ width: "900px" }}
+        >
+          <div className="bg-body-tertiary p-4 pt-2 border rounded-3">
+            <h1 className="display-3 fw-bold">Anime List Share</h1>
+            <img
+              src={animeCharacters}
+              className="rounded w-100 mb-2 object-fit-cover cover-image"
+              alt="anime characters"
+            />
+            <p className="fs-3 m-0 lh-sm">
+              Discover, Create and Share Your Anime Lists with all the anime
+              addicts and the community. <br />
+              {userData?.username ? (
+                <Button variant="primary" className="mt-3">
+                  <Link
+                    to="/dashboard"
+                    className="text-decoration-none text-white fw-semibold"
+                  >
+                    Go To Dashboard
+                  </Link>
+                </Button>
+              ) : (
+                <Button variant="primary" className="mt-3">
+                  <Link
+                    to="/register"
+                    className="text-decoration-none text-white fw-semibold"
+                  >
+                    Register To Begin
+                  </Link>
+                </Button>
+              )}
+            </p>
+          </div>
+          {/*  */}
+          <div className="bg-body-tertiary p-4 border rounded-3">
+            <p className="m-0 fs-4">
+              Visit the{" "}
+              <Link to="/search" className="text-decoration-none fw-bold">
+                Search
+              </Link>{" "}
+              page and start adding animes to your lists.
+              <br />
+            </p>
+          </div>
+          {/*  */}
+          <div className="row lists-info">
+            <div className="col-md-6">
+              <div className="p-4 bg-danger bg-gradient border rounded-3">
+                <h2>🔒 Private Lists</h2>
+                <p className="fs-5">
+                  Create <span className="fst-italic fw-bold">Private </span>
+                  lists of animes of your favorites or guilty pleasures which
+                  cannot be viewed by anyone.
+                </p>
+                {userData?.username && (
+                  <Button
+                    variant="dark"
+                    className="list-btn"
+                    onClick={privateBtnHandler}
+                  >
+                    <Link
+                      to="/lists"
+                      className="text-decoration-none text-danger fw-bold"
+                    >
+                      Your Private Lists
+                    </Link>
+                  </Button>
+                )}
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="p-4 bg-success bg-gradient border rounded-3">
+                <h2>👁️ Public Lists</h2>
+                <p className="fs-5">
+                  Create <span className="fst-italic fw-bold">Public </span>
+                  lists of your top picks and recommendations to share with your
+                  friends or with the community.
+                </p>
+                {userData?.username && (
+                  <Button
+                    variant="dark"
+                    className="list-btn"
+                    onClick={publicBtnHandler}
+                  >
+                    <Link
+                      to="/lists"
+                      className="text-decoration-none text-success fw-bold"
+                    >
+                      Your Public Lists
+                    </Link>
+                  </Button>
+                )}
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
